9d28f743c052594a70c9434e2f7af16505c499f9
Vadym Chekan <kot.begemot@gmail.com>
2020-05-22 22:17:30 -0700

    Cleanup

--- src/murmur2a.rs
+++ src/murmur2a.rs
@@ -10,22 +10,22 @@ assert_eq!(Hash32A::hash(b"hello"), 259931098);
 /// Not the fastest implementation, because works with individual bytes, but should 
 /// be ok for intendend use: keys hash calculation.
 pub fn hash32(data: &[u8]) -> u32 {
-    const seed: u32 = 0x9747b28c;
-    const m: u32 = 0x5bd1e995;
-    const r: u32 = 24;
+    const SEED: u32 = 0x9747b28c;
+    const M: u32 = 0x5bd1e995;
+    const R: u32 = 24;
     let len = data.len();
 
-    let mut h = seed ^ len as u32;
+    let mut h = SEED ^ len as u32;
 
     for i in 0..len/4 {
         let i4 = i * 4;
         let mut k = data[i4 + 0] as u32 + (data[i4 + 1] as u32) << 8 +
             (data[i4 + 2] as u32) << 16 + (data[i4 + 3] as u32) << 24;
 
-        k *= m;
-        k ^= k >> r;
-        k *= m;
-        h *= m;
+        k *= M;
+        k ^= k >> R;
+        k *= M;
+        h *= M;
         h ^= k;
     }
 
@@ -39,12 +39,12 @@ pub fn hash32(data: &[u8]) -> u32 {
     }
     if remaining >= 1 {
         h ^= data[(len & !3)] as u32;
-        h *= m;
+        h *= M;
 
     }
 
     h ^= h >> 13;
-    h *= m;
+    h *= M;
     h ^= h >> 15;
 
     h    
